501bad57a1eb2037d679ee1bff2faa76b69ad205
Brack <brackallen5271@gmail.com>
2026-02-08 12:49:50 -0600

    update

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return jsonify({"status": "backend alive"})
+
 @app.route("/ping")
 def ping():
     return jsonify({"pong": True})
